63c01dee214a84f8e2b5c11b96a46fcb8c43d310
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-23 23:23:52 -0700

    Create reverse_list.py

---
+++ reverse_list.py
@@ -0,0 +1,6 @@
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+reversed_numbers = numbers[::-1]
+
+print("Original list:", numbers)
+print("Reversed list:", reversed_numbers)
